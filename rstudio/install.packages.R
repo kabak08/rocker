1897002f6dd301bbfee894d14746319ead301c87
kabak08 <kabak08@users.noreply.github.com>
2017-11-22 08:01:34 -0500

    updated installed packages

--- rstudio/install.packages.R
+++ rstudio/install.packages.R
@@ -1,1 +1,27 @@
-install.packages(c('tidyverse', 'data.table','odbc'))
+packages <- c('abind','acepack','actuar','ActuDistns','AER','alr3','arm','ash','assertthat','astsa','audio','backports','base64enc','batch','bayesplot','BB','BBmisc',
+              'bcp','beepr','bezier','BH','bigdata','biglm','bigmemory','bigmemory.sri','bitops','blob','boot','BradleyTerry2','brew','brglm','broom','car','caret',
+              'caTools','cellranger','ChainLadder','checkmate','chron','circular','class','cluster','coda','codetools','coin','colorspace','colourpicker','combinat',
+              'compute.es','contfrac','corrgram','cplm','crayon','curl','cvAUC','data.table','DBI','dendextend','DEoptimR','deSolve','devtools','dfoptim','dichromat',
+              'digest','diptest','doMPI','doParallel','doSNOW','dplyr','drc','DT','dygraphs','e1071','easypackages','effects','elliptic','entropy','evaluate','expint','fail',
+              'fda','flexmix','FNN','forcats','foreach','forecast','foreign','formatR','Formula','fortunes','fpc','fracdiff','functional','gbm','gclus','gdata','gender','geosphere',
+              'getopt','ggmap','ggplot2','ggpmisc','git2r','glmnet','gmodels','gmp','googleVis','gplots','gridBase','gridExtra','gsl','gstat','gsubfn','gtable','gtools','h2o',
+              'HadoopStreaming','hash','haven','hexbin','highr','histogram','hms','htmltools','htmlwidgets','httpuv','httr','hunspell','HydroMe','hypergeo','igraph',
+              'inline','intervals','ipred','irlba','iterators','itertools','janeaustenr','jpeg','jsonlite','kernlab','KernSmooth','knitr','kohonen','labeling','lattice','latticeExtra',
+              'lava','lazyeval','lda','leaps','lhs','limSolve','lme4','lmtest','locfit','loo','lpSolve','lubridate','magic','magrittr','manipulate','mapproj','maps','maptools',
+              'markdown','MASS','Matching','Matrix','MatrixModels','matrixStats','maxent','mclust','memoise','mgcv','mime','miniUI','minpack.lm','minqa','misc3d',
+              'mnormt','ModelMetrics','modelr','modeltools','moments','multcomp','multicool','munsell','mvcwt','mvtnorm','network','NISTnls','nlme','nloptr',
+              'NLP','nls2','nlstools','NMF','NMOF','nnet','nnls','numDeriv','objectSignals','odbc','openNLP','openNLPdata','openssl','openxlsx','optextras','optimx',
+              'packrat','party','pastecs','pbdMPI','pbkrtest','pkgmaker','PKI','plogr','plotrix','plumber','plumbr','plyr','png','poisson.glm.mix','polspline','polynom',
+              'prabclus','praise','pROC','prodlim','profileModel','proto','psych','purrr','pwr','qap','qdap','qdapDictionaries','qdapRegex','qdapTools','quadprog',
+              'quantreg','R2G2','R6','RAdwords','randaes','randomForest','Rcgmin','RColorBrewer','Rcpp','RcppArmadillo','RcppEigen','RCurl','Rdsm','readODS',
+              'readr','readxl','registry','reliaR','rematch','reports','reshape2','rgenoud','RgoogleMaps','rJava','rjson','RJSONIO','rlecuyer','rmarkdown','R.methodsS3',
+              'Rmpfr','Rmpi','rngtools','robustbase','ROCR','RODBC','RODM','R.oo','rpart','rprojroot','rsconnect','Rserve','RSQLite','rstan','rstantools','rstudioapi',
+              'RTextTools','RUnit','R.utils','rversions','rvest','Rvmmin','Rwave','sandwich','sas7bdat','scales','scatterplot3d','sde','selectr','sendmailR','seriation','setRNG',
+              'shapefiles','shiny','shinyjs','shinystan','shinythemes','signal','slam','smoother','sna','snow','SnowballC','som','sourcetools','sp','spacetime','SparseM','spatial',
+              'splus2R','sqldf','squash','StanHeaders','stargazer','statmod','statnet.common','stringdist','stringi','stringr','strucchange','subsemble','SuperLearner','survival',
+              'svUnit','systemfit','tau','testthat','TH.data','threejs','tibble','tidyr','tidytext','tidyverse','timeDate','tm','tokenizers','topicmodels','tree','trimcluster','TSA',
+              'tseries','TSP','TTR','tuneR','tweedie','ucminf','venneuler','viridis','visualize','wfe','whisker','withr','wle','wordcloud','xlsx','xlsxjars','XML','xml2','xtable','xts',
+              'yaml','zipcode','zoo','base','boot','class','cluster','codetools','compiler','datasets','foreign','graphics','grDevices','grid','KernSmooth','lattice','MASS','Matrix',
+              'methods','mgcv','nlme','nnet','parallel','rpart','spatial','splines','stats','stats4','survival','tcltk','tools','utils')
+
+install.packages(packages)
